49529a9dba974eff7cf55cb1f070a065bebd7ac2
Matthew Leeds <mwl458@gmail.com>
2015-11-15 23:12:56 -0600

    Exit from the cracking script once all wallets have been decrypted.

--- crack.sh
+++ crack.sh
@@ -3,10 +3,16 @@
 # This script reads passwords.txt line by line and prints any lines that
 # successfully decrypt the passwords stored by password_keeper
 
+let "NUMBER_WALLETS=`ls -l ~/.wallets | wc -l`-1"
+declare NUMBER_FOUND=0
 while read -r line; do
     echo "$line" | python3 main.py > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "$line"
+        let "NUMBER_FOUND++"
+        if [ $NUMBER_FOUND -eq $NUMBER_WALLETS ]; then
+          exit
+        fi
     fi
 done < "passwords.txt"
 
